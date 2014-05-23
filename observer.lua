4fe2f0f9448c9da9856bd4d1b817cc59602fabc4
Daniel Shumway <shumway.danny@gmail.com>
2014-05-23 13:06:28 -0400

    Adding in readme info for different folders, which I'll edit online.

--- observer.lua
+++
@@ -1,7 +0,0 @@
---We make a recorder object.
---This allows us to keep track of everything that the script does.
-recorder = {}
-
---I want to be able to save to a file.
---I want the recorder to record multiple things at the same time.
---I want the recorder to be able to deal with possibly being cut off at any time.

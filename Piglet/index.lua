a63433c2c8a16be56780b72ce867a873b2b55cb9
Daniel Shumway <shumway.danny@gmail.com>
2014-10-09 18:48:08 -0400

    Deleted a blank index, commented current index.
    Signed-off-by: Daniel Shumway <shumway.danny@gmail.com>

--- Piglet/index.lua
+++ Piglet/index.lua
@@ -1,4 +1,4 @@
-print('hello')
+print("Hai, I'm Piglet")
 
 
 --[[
@@ -41,9 +41,8 @@ and stores that in instant memory as well.
 Piglet.Memory = {}
 Piglet.Memory.Instant = dofile('memory/Instant/instant.lua')
 Piglet.Memory.Short = dofile('memory/Short/short.lua')
-Piglet.Memory.Long = dofile('memory/Long/long.lua')
-
 --We don't have an accessible long.  What long should be is a class that pulls data and puts it into short.
+Piglet.Memory.Long = dofile('memory/Long/long.lua')
 
 Piglet.Hardware = {}
 Piglet.Hardware.Eye = dofile('Hardware/eye.lua')
@@ -55,10 +54,23 @@ xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
               MENTAL PROCESSES
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
+Piglet's brain is divided into three separate parts that work together.
+
+An Observer looks at the world and gathers information - it's designed to replicate the part 
+of your brain that would work in conjunction with an eye, filling in gaps where it needs to, logging
+changes that it sees in the world, etc...
+
+The processor, suitably enough, processes information that is passed in from the observer and forms conclusions
+about the world.
+
+The decider, using infomration that is stored in short term memory passes inputs to hardware like the hand about
+what should be done in the future.  So the decider would look at instant/short memory and make a decision:
+"I should press left", then it will pass 'left' to the hand, which will in the next frame, hold down left.
+It works kind of a nervous system.
 -----------------------------------------------------------------------------------
 --]]
 
---Should probably be a brain ({}) object here.  Maybe add one in the future.
+--ToDo: Should probably be a brain ({}) object here.  Maybe add one in the future?
 Piglet.Observer = dofile('Observers/observer.lua')
 Piglet.Processor = dofile('Processors/processor.lua')
 Piglet.Decider = dofile('Deciders/decider.lua')
@@ -66,7 +78,7 @@ Piglet.Decider = dofile('Deciders/decider.lua')
 
 --[[
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-              MENTAL PROCESSES
+              MAIN LOOP
 xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 -----------------------------------------------------------------------------------
@@ -85,7 +97,8 @@ end
 
 --Draw any messages required to the screen.  
 --Do memory display or whatever the heck you want here as well.
---Note that this is not attached to Piglet.
+--Note that this is not attached to Piglet.  Piglet is not actively drawing to the screen.
+--We are, after pulling information from piglet.
 function draw()
 
 	local toPrint = ' '
@@ -100,11 +113,15 @@ end
 
 
 --main
---Remember some stuff.
+--Remember some stuff.  Legacy code from when Piglet was storing and remembering stuff.
 --Piglet.Memory.Short.rememberCauses(Piglet.Memory.Long.load("Mario_6Golden_Coins", "test_01"))
---Should probably move this.
-Piglet.Memory.Short.strategies.init(8, 6)
-Piglet.Memory.Short.recordInitialState()
+
+Piglet.Memory.Short.strategies.init(8, 6) --When piglet boots up, we start up her memory.  In the future, we would have some type of recall function
+--That would be tied into long in the future.
+
+Piglet.Memory.Short.recordInitialState() --Check out what's going on in the game.
+
+--And here we go.
 while(true) do
 	--Piglet sees.
 	--Eye
